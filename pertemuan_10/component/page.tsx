f25b315f49fed34ce536cd4fc4df41b0acd2597f
brendaabgl <brendaabgl@gmail.com>
2025-10-14 08:03:59 +0700

    Initial commit

---
+++ pertemuan_10/component/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+// import React from "react";
+import HeroSection from "./herosection";
+import WorkSection from "./worksection";
+import ServiceSection from "./servicesection";
+import ContactSection from "./contactsection";
+
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <div>
+      <HeroSection />
+      <Container className="my-5">
+        <Row className="my-4">
+          <Col md={6} className="align-self-center mb-3">
+            <h6>What We Do?</h6>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat,
+              ipsam culpa. Commodi aspernatur temporibus placeat libero fugit at
+              sequi consequuntur.
+            </p>
+            <Button variant="info">Learn More</Button>
+          </Col>
+          <Col md={6}>
+            <Image
+              src="/images/about.jpg"
+              alt="About Us"
+              width={500}
+              height={500}
+              className="img-fluid"
+            />
+          </Col>
+        </Row>
+      </Container>
+      <section className="py-5">
+        <WorkSection />
+      </section>
+      <ServiceSection />
+      <section id="contact">
+        <ContactSection />
+      </section>
+    </div>
+  );
+};
+
+export default page;
